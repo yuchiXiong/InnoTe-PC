e8bed6a5600c35b45433371413441cad14927b92
yuchiXiong <yuchi.xiong@foxmail.com>
2023-07-16 16:34:38 +0800

    feat: 添加文件列表选中态

--- src/pages/components/directory.tsx
+++ src/pages/components/directory.tsx
@@ -13,6 +13,7 @@ const Directory = (props: IDirectoryProps) => {
 
   const { dir, level = 1 } = props;
   const [extendRecord, setExtendRecord] = useState<Record<string, boolean>>({});
+  const [current, setCurrent] = useState<IDirectory | null>(null);
 
   /** 是目录 */
   const isDir = (dir: IDirectory): boolean => 'children' in dir;
@@ -23,13 +24,16 @@ const Directory = (props: IDirectoryProps) => {
   /** 是否展开 */
   const isExtend = (dir: IDirectory): boolean => extendRecord[dir.path];
 
+  /** 是否选中 */
+  const isSelected = (dir: IDirectory): boolean => current?.path === dir.path;
+
   /** 渲染图标 */
   const showIcon = (item: IDirectory) => {
     if (!isDir(item))
-      return <Notes theme="filled" size="16" fill="#999" />;
+      return <Notes theme="filled" size="16" fill="#999"/>;
     return extendRecord[item.path]
-      ? <FolderOpen theme="filled" size="16" fill="#666" />
-      : <FolderClose theme="filled" size="16" fill="#999" />;
+      ? <FolderOpen theme="filled" size="16" fill="#666"/>
+      : <FolderClose theme="filled" size="16" fill="#999"/>;
   };
 
   const handleItemClick = (item: IDirectory) => {
@@ -39,6 +43,8 @@ const Directory = (props: IDirectoryProps) => {
         ...extendRecord,
         [item.path]: !extendRecord[item.path]
       });
+    } else {
+      setCurrent(item);
     }
   };
 
@@ -47,11 +53,14 @@ const Directory = (props: IDirectoryProps) => {
       {(dir.children || []).map((child) => (
         <React.Fragment key={child.path}>
           <li
-            className={`flex items-center w-full px-4 py-2 from-zinc-200 backdrop-blur-2xl ${isRoot(child) && isExtend(child) && 'border-b border-dashed border-gray-400'}`}
+            className={`flex items-center w-full px-4 py-2 from-zinc-200 
+            backdrop-blur-2xl cursor-pointer transition 
+            ${isRoot(child) && isExtend(child) && 'border-b border-dashed border-gray-400'}
+            `}
             onClick={() => handleItemClick(child)}
           >
             {showIcon(child)}
-            <p className='ml-2 font-sans text-base font-thin cursor-pointer line-clamp-1'>{showTitle(child.name)}</p>
+            <p className={`ml-2 font-sans text-base font-thin line-clamp-1 ${isExtend(child) && 'font-medium'} ${isSelected(child) && 'text-green-500'}`}>{showTitle(child.name)}</p>
           </li>
           {isExtend(child) && isDir(child) && (
             <section className='pl-4'>
@@ -64,7 +73,7 @@ const Directory = (props: IDirectoryProps) => {
           )}
         </React.Fragment>
       ))}
-    </ul >
+    </ul>
   )
 }
 
